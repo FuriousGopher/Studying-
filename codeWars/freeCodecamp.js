c385708983b79b57fd7082ebe79f49b32a7f8dbb
Ivan Kabar <alkazavr94@google.com>
2023-03-12 14:30:34 +0100

    ...arg

--- codeWars/freeCodecamp.js
+++ codeWars/freeCodecamp.js
@@ -72,3 +72,15 @@ const myConcat = (arr1, arr2) => arr1.concat(arr2);
 
 
 console.log(myConcat([1, 2], [3, 4, 5]));
+
+
+///////////////////////////
+
+const sum = (...args) => {
+    let total = 0;
+    for (let i = 0; i < args.length; i++) {
+        total += args[i];
+    }
+    return total;
+}
+console.log(sum(1, 2, 3)); // 6
