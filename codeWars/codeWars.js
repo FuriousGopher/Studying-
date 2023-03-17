5dfbc974c52c48c226a3cf8df61fa145de6d215e
Ivan Kabar <alkazavr94@google.com>
2023-03-17 12:58:13 +0100

    walk

--- codeWars/codeWars.js
+++ codeWars/codeWars.js
@@ -54,3 +54,34 @@ function heron(a, b, c) {
 /////////////////////////////
 
 const doubleChar = (str) => str.split("").map(c => c + c).join("");
+
+//////////////////////////////
+function isValidWalk(walk) {
+    if (walk.length !== 10) {
+        return false; // the walk is not 10 minutes long
+    }
+
+    let x = 0;
+    let y = 0;
+
+    for (let i = 0; i < walk.length; i++) {
+        switch (walk[i]) {
+            case 'n':
+                y++;
+                break;
+            case 's':
+                y--;
+                break;
+            case 'e':
+                x++;
+                break;
+            case 'w':
+                x--;
+                break;
+            default:
+                return false;
+        }
+    }
+
+    return x === 0 && y === 0;
+}
