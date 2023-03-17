150775b34a4f2ca45692e48736373564fa548d6a
Ivan Kabar <alkazavr94@google.com>
2023-03-17 12:42:31 +0100

    .map

--- codeWars/codeWars.js
+++ codeWars/codeWars.js
@@ -50,3 +50,7 @@ function heron(a, b, c) {
     var s = (a + b + c) / 2;
     return Math.sqrt(s * (s - a) * (s - b) * (s - c));
 }
+
+/////////////////////////////
+
+const doubleChar = (str) => str.split("").map(c => c + c).join("");
