32312f3a49c22045862da01ec71fa250df883fdb
Ivan Kabar <alkazavr94@google.com>
2023-03-25 00:42:38 +0100

    sever js

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
-let counter = 0;
+const http = require("http")
+let requestCount = 0
 
-setInterval(()=>{
-    counter++
-    console.log(counter)
-}, 1000)
+const server = http.createServer((request, response) => {
+
+    switch (request.url){
+        case '/students':
+            response.write("  students")
+            break;
+        case '/back':
+            response.write("  back")
+            break;
+        default:
+            response.write("  404 not found")
+    }
+
+
+    requestCount++
+    response.write("     Hello im new Server"    +requestCount)
+    response.end()
+})
+
+server.listen(3003)
