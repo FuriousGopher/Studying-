5224c5f7eeebf4e3676f72c1781b2e1b458c88a4
Ivan Kabar <alkazavr94@google.com>
2023-03-27 14:41:17 +0200

    Back End lessons

---
+++ Back-End/dist/__tests__/e2e/course.api.test.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const node_test_1 = require("node:test");
+(0, node_test_1.describe)('/course', () => {
+    it('should return 200 and empty array', () => {
+    });
+});
---
+++ Back-End/dist/src/index.js
@@ -0,0 +1,68 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+const port = 3000;
+const jsonBodyMiddleware = express_1.default.json();
+app.use(jsonBodyMiddleware);
+const db = {
+    courses: [
+        { id: 1, title: 'front-end' },
+        { id: 2, title: 'back-end' },
+        { id: 3, title: 'auto qa' },
+        { id: 4, title: 'devops' }
+    ]
+};
+app.get('/', (req, res) => {
+    const a = 4;
+    if (a > 5) {
+        res.send('OK!');
+    }
+    else {
+        res.send('Hello World!');
+    }
+});
+app.get('/courses', (req, res) => {
+    let FoundCourses = db.courses;
+    if (req.query.title) {
+        FoundCourses = FoundCourses.filter(c => c.title.indexOf(req.query.title) > -1);
+    }
+    if (!FoundCourses.length) {
+        res.sendStatus(404);
+        return;
+    }
+    res.json(FoundCourses);
+});
+app.get('/courses/:id', (req, res) => {
+    2;
+    const foundCourse = db.courses.find(c => c.id === +req.params.id);
+    if (!foundCourse) {
+        res.sendStatus(404);
+        return;
+    }
+    res.json(foundCourse);
+});
+app.get('/users', (req, res) => {
+    res.send(' Samurais!');
+});
+app.post('/users', (req, res) => {
+    res.send('We create Samurais!');
+});
+app.post('/courses', (req, res) => {
+    const createdCours = {
+        id: +(new Date()),
+        title: 'unknown'
+    };
+    db.courses.push(createdCours);
+    res.json(createdCours);
+});
+app.delete('/courses/:id', (req, res) => {
+    db.courses = db.courses.filter(c => c.id !== +req.params.id);
+    res.sendStatus(204);
+});
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
